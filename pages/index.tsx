0bd6d5d02189586e8c60e5fbc66b41fe57f367c6
elianrichard <elianrichards@gmail.com>
2022-10-15 22:23:09 +0700

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center bg-cyan-300">
+      <div className="text-5xl font-bold">Biel cantik</div>
+    </div>
+  );
+};
+
+export default Home;
