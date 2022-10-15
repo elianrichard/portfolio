73e3abd1bdb239aee3275b3855573ddf92caa23c
elianrichard <elianrichards@gmail.com>
2022-10-15 23:08:20 +0700

    edited index.js

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,7 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return (
-    <div className="flex h-screen w-screen items-center justify-center bg-cyan-300">
-      <div className="text-5xl font-bold">Biel cantik</div>
-    </div>
-  );
+  return <div className="flex">Hello World!</div>;
 };
 
 export default Home;
