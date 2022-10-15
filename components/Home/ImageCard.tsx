48b4b847ee255f5689e7606c3388816b8cf5610c
elianrichard <elianrichards@gmail.com>
2022-10-16 01:29:28 +0700

    refined landing animation

--- components/Home/ImageCard.tsx
+++ components/Home/ImageCard.tsx
@@ -11,12 +11,12 @@ const ImageCard = ({ image, isSelected, setSelected }: Props) => {
   return (
     <div
       className={`relative h-full w-full ${
-        isSelected ? "min-w-[1000px]" : "min-w-[0]"
+        isSelected ? "max-w-[900px]" : "max-w-[150px]"
       } transition-all duration-300 ease-in-out`}
       onMouseEnter={setSelected}
     >
-      <div className={`${isSelected ? "bg-black/0" : "bg-black/40"} absolute w-full h-full z-10 transition-bg duration-300 ease-in-out`}/>
-      <Image src={image} alt="Scenery" layout="fill" objectFit="cover" className={`${isSelected ? "scale-110" : "scale-100"} transition-transform duration-1000 ease-in-out`}/>
+      <div className={`${isSelected ? "bg-black/0" : "bg-black/40"} absolute w-full h-full z-10 transition-bg duration-700 ease-in-out`}/>
+      <Image src={image} alt="Scenery" layout="fill" objectFit="cover" className={`${isSelected ? "scale-105" : "scale-100"} transition-transform duration-1000 ease-in-out`}/>
     </div>
   );
 };
