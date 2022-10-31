e5b20b9c759b6bb28ae0a3e1ecb9e919fe1eb249
elianrichard <elianrichards@gmail.com>
2022-10-31 18:35:02 +0700

    renamed domain

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "http://localhost:3000"
-  : "https://portfolio-theta-red-61.vercel.app";
+  : "https://portfolio-alpha-rho-topaz.vercel.app/";
